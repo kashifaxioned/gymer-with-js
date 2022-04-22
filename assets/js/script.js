be38547adcc937730c84d198d07334ded767f8f3
kashifaxioned <kashifa@axioned.com>
2022-04-22 09:41:37 +0530

    slick slider added for card section

--- assets/js/script.js
+++ assets/js/script.js
@@ -108,3 +108,40 @@ $(".gym-slider").slick({
   ]
 })
 
+// slick slider card section
+
+$(".card-section").slick({
+  autoplay: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
+
+
