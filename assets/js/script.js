f73dc0a24d8825e5b959013d2f4c053d7c50b117
kashifaxioned <kashifa@axioned.com>
2022-04-22 09:40:51 +0530

    slick slider added for gym-slider  added

--- assets/js/script.js
+++ assets/js/script.js
@@ -72,3 +72,39 @@ let observer = new IntersectionObserver((entries) => {
 })
 observer.observe(counterSection)
 
+// slick slider gym slider
+
+$(".gym-slider").slick({
+  autoplay: true,
+  arrows: false,
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  responsive: [{
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+})
+
